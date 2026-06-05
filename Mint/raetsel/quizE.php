85f54b4e1d66b9d009ea0600f2017b95acf2aa47
H26949 <helena.bach@tms-hl.org>
2026-06-05 10:19:31 +0200

    Add files via upload
    fertige Version

---
+++ Mint/raetsel/quizE.php
@@ -0,0 +1,45 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="test.css">
+
+</head>
+<body>
+
+  <nav>
+    <div class="element">Home</div>
+    <div class="element">Scoreboard</div>
+    <div class="element">News</div>
+    <div class="element no-border dropdown">
+      Dropdown
+      <div class="dropdown-content">
+        <button>Link 1</button>
+        <button>Link 2</button>
+        <button>Link 3</button>
+      </div>
+    </div>
+  </nav>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Geschafft!</title>
+
+
+</head>
+<body>
+    <div class="card">
+
+        <h1>Super, du hast diese Rätselgeschichte abgeschlossen!</h1>
+<p align="center" >
+<a href="quiz_Startseite.html" class="button">Zur&uuml;ck zur Startseite </a>
+</p>
+    </div>
+</body>
+</html>
---
+++ Mint/raetsel/quizFalsch.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="test.css">
+
+</head>
+<body>
+
+  <nav>
+    <div class="element">Home</div>
+    <div class="element">Scoreboard</div>
+    <div class="element">News</div>
+    <div class="element no-border dropdown">
+      Dropdown
+      <div class="dropdown-content">
+        <button>Link 1</button>
+        <button>Link 2</button>
+        <button>Link 3</button>
+      </div>
+    </div>
+  </nav>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Falsche Antwort</title>
+</head>
+<body>
+    <div class="card">
+        <h1><font color="Red">Falsche Antwort</font></h1>
+
+        <div class="question">
+        Das ist leider falsch. Vielleicht liegt dir eine andere Geschiche mehr? Probiere es aus!
+
+        </div>
+
+                 <p align="center" >
+<a href="quiz_Startseite.html" class="button">Zur&uuml;ck zur Startseite </a>
+</p>
+
+</body>
+</html>
