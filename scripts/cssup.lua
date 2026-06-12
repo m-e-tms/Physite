b67edb9d226d78aeef6e95b59fa026f0b3aeb3d9
m-e-tms <maurice.eggers@tms-hl.org>
2026-06-12 10:24:04 +0200

    css verlinkung

---
+++ scripts/cssup.lua
@@ -0,0 +1,23 @@
+#!lua
+
+if (#arg ~= 2) then
+    print("error: incorrect number of arguments\nusage: ./cssup.lua <source_file_name>.html <destination_file_name>.html")
+    os.exit(1)
+end
+
+local css_pattern = "<link rel=\"stylesheet\" href=\".-\">"
+
+local source_file = io.open(arg[1], "r")
+local css_string = source_file:read("a"):match(css_pattern)--:gsub("[^\n]-\n", "  %0")
+source_file:close()
+
+css_string = "<link rel=\"stylesheet\" href=\"../../Physite/scripts/test.css\">"
+
+local destination_file = io.open(arg[2], "r")
+local content = destination_file:read("a")
+destination_file:close()
+
+destination_file = io.open(arg[2], "w")
+destination_file:write(content:gsub(css_pattern, css_string))
+destination_file:flush()
+destination_file:close()
---
+++ scripts/cssupall.lua
@@ -0,0 +1,15 @@
+#!lua
+
+if (#arg ~= 1) then
+    print("error: incorrect number of arguments\nusage: ./cssupall.lua <source_file_name>.html")
+    os.exit(1)
+end
+
+local result = io.popen("find Oberstufenthemen -name \"*.html\"", "r")
+
+for line in result:lines() do
+    arg[2] = line
+    dofile("scripts/cssup.lua")
+end
+
+result:close()
